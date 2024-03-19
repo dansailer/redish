d2ecdf2ad6565cd59a19fc3a918e3c489ccc8646
dansailer <19692206+dansailer@users.noreply.github.com>
2024-03-19 13:24:30 +0100

    Disabling dynamic linking of C code on Linux
    CGO_ENABLED=0

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	version     = "0.0.2"
+	version     = "0.0.3"
 	showHelp    = flag.Bool("help", false, "Output this help and exit.")
 	showVersion = flag.Bool("version", false, "Output version and exit.")
 	uri         = flag.String("uri", "localhost:6379", "Redis server URI")
